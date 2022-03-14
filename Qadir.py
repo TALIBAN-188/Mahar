0054f7ecf3599898952312d64cb18c5d559e1e35
Qadirking <87000895+Qadirking@users.noreply.github.com>
2022-03-14 18:13:32 +0500

    Update Qadir.py

--- Qadir.py
+++ Qadir.py
@@ -3,7 +3,7 @@ try:
     import os,sys,subprocess,requests
 except ModuleNotFoundError:
     os.system('pip install requests futures bs4')
-    os.system('python aoun.py')
+    os.system('python Qadir.py')
 current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('a64'):
